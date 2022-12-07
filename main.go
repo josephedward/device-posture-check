19ddb7dc65dd16bce8c1029fbe90aa840135c6b1
josephedward <15126922+josephedward@users.noreply.github.com>
2022-12-07 16:31:53 -0500

    client

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
+
 package main
 
 import (
+	// "encoding/json"
 	"context"
-	"fmt"
-	"github.com/tailscale/tailscale-client-go/tailscale"
 	"godpc/cli"
-	"github.com/hokaccha/go-prettyjson"
+	"fmt"
+	// "github.com/hokaccha/go-prettyjson"
+	"github.com/tailscale/tailscale-client-go/tailscale"
+	//tsnet 
+	"github.com/tailscale/tailscale-client-go/tailscale/net"
+
+	
 )
 
+
+/**
+ * SECOND HALF OF THE DPC PROCESS
+ */
 func main() {
 
 	tsenv, err := Env()
@@ -20,13 +30,35 @@ func main() {
 
 	// List all your devices
 	devices, err := client.Devices(context.Background())
-	cli.Success("devices :",devices)
+	cli.Success("device :",devices[0])
 	// loop over devices
-	// for _, device := range devices {
-	// 	fmt.Println(device)
-	// }
+	for _, device := range devices {
+		fmt.Println(device.Name)
+	}
 	cli.PrintIfErr(err)
-	s, _ := prettyjson.Marshal(devices)
-	fmt.Println(string(s))
+
+	// s, _ := prettyjson.Marshal(devices)
+	// fmt.Println(string(s))	
+	
+	//open an ssh connection to a device
+	conn, err := client.Dial(context.Background(), "device-name")
+	cli.PrintIfErr(err)
+	defer conn.Close()
+
+	//run a command on the device
+	cmd := exec.Command("ls", "-l")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = conn
+	err = cmd.Run()
+	cli.PrintIfErr(err)
+
+	//get the device's current status
+	status, err := client.Status(context.Background())
+	cli.PrintIfErr(err)
+	fmt.Println(status)
+
+	
 }
   
+// osqueryi --json "select * from os_version;"

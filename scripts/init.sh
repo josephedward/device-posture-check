689af676a4bb98cb0fcf21b362354c18c4a37c09
josephedward <15126922+josephedward@users.noreply.github.com>
2022-12-16 00:30:58 -0500

    Squashed commit of the following:
    commit 961b58dd90e5fb4681b08584e9f9624acfb16a1e
    Author: josephedward <15126922+josephedward@users.noreply.github.com>
    Date:   Fri Dec 16 00:30:15 2022 -0500
    
        WIP - ui for querying/authing node

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
+TSIP=$(tailscale ip -4) 
+echo TSIP : $TSIP
+echo TSIP=\"$TSIP\" >> .env
 
-echo TSIP=\"$(tailscale ip -4)\" >> .env
-echo $TSIP
-echo "y" | sudo apt install jq
-echo "y" | sudo apt install gcc
-echo "y" | sudo apt install g++
-echo DEVID=\"$(tailscale status --json | jq -r '.Self.UserID'  )\" >> .env
-echo $DEVID
-
+DEVID=$(tailscale status --json | jq -r '.Self.UserID')
+echo DEVID : $DEVID
+echo DEVID=\"$DEVID\" >> .env
 

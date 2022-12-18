3ee489be9bf2ffd236b40b8aa6c12e8c8b7aea4f
josephedward <15126922+josephedward@users.noreply.github.com>
2022-12-17 23:20:50 -0500

    Update service.go
    createlistener() alternate approach to serving via tsnet

--- tailscale/service.go
+++ tailscale/service.go
@@ -9,10 +9,14 @@ import (
 	"net/http"
 	"strings"
 	"tailscale.com/tsnet"
+	"crypto/tls"
+	"os"
+	// "tailscale.com/client/tailscale"
+	// "github.com/henvic/httpretty"
 )
 
 var (
-	hostname = flag.String("hostname", "hello", "hostname for the tailnet")
+	hostname = flag.String("hostname", "PostureService", "hostname for the tailnet")
 )
 
 func CreateService(statusQuery string, env cli.TsEnv) {
@@ -49,4 +53,54 @@ func CreateService(statusQuery string, env cli.TsEnv) {
 
 	}))
 
+}
+
+
+func CreateListener() {
+	// os.Setenv("TAILSCALE_USE_WIP_CODE", "true")
+	// TODO: comment out this line to avoid having to re-login each time you start this program
+	// os.Setenv("TS_LOGIN", "1")
+	
+
+	os.Setenv("HOME", "./perm/postureservice")
+	
+	// hostname := flag.String("hostname", "josephedward.github", "tailscale hostname")
+	allowedUser := flag.String("josephedward@github", "", "the name of a tailscale user to allow")
+	flag.Parse()
+	s := &tsnet.Server{
+		Hostname: *hostname,
+	}
+	
+	log.Printf("starting tailscale listener on hostname %s", *hostname)
+	ln, err := s.Listen("tcp", ":443")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//create local client
+	LocalClient, err := s.LocalClient()
+	cli.PrintIfErr(err)
+	
+	ln = tls.NewListener(ln, &tls.Config{
+		GetCertificate: LocalClient.GetCertificate,
+		
+	})
+
+	defer ln.Close()
+
+	http.Serve(ln, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		who, err := LocalClient.WhoIs(r.Context(), r.RemoteAddr)
+		if err != nil {
+			cli.Success("here")
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		if who.UserProfile.LoginName != *allowedUser {
+			cli.Success("here2")
+			http.Error(w, "not allowed", 403)
+			return
+		}
+		cli.Success("here3")
+		fmt.Fprintf(w, "Hello, %s", html.EscapeString(who.UserProfile.LoginName))
+	}))
 }
